e7fd017ad6d90d9f760f75113de0a03acbd85498
Sam Spieth <32369371+samspieth@users.noreply.github.com>
2019-04-24 10:04:28 -0700

    upload phue blink python script

---
+++ phue_blink_all.py
@@ -0,0 +1,29 @@
+from phue import Bridge
+from time import sleep
+#import random
+
+#b = Bridge('10.0.1.16')    #Spieth Home
+b = Bridge('172.28.219.225') #Lab Green
+
+# If the app is not registered and the button is not pressed, press the button and call connect() (this only needs to be run a single time)
+#b.connect()
+
+# Get the bridge state (This returns the full dictionary that you can explore)
+#print(b.get_api())
+
+
+lights = b.get_light_objects()
+
+
+for light in lights:
+    print(light)
+    light.transitiontime = 0;
+    
+while True:
+    for light in lights:
+        light.on = True
+        light.brightness = 255
+        light.xy = [1,.5]
+        sleep(.1)
+        light.on = False
+        sleep(.1)
